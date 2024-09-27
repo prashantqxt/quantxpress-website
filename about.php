8f438586d42fcd888a5facbd7fced3d3b0dd73b5
Deepak <deepak.verma@straltoglobal.com>
2024-09-27 23:01:33 +0530

    New

--- about.php
+++ about.php
@@ -9,7 +9,30 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/style.css">
+  <style type="text/css">
+      @media (max-width:600px){
+        .height-58 {
+            height: 85vh;
+        }
+      }
+      @media (min-width:600px) and (max-width:786px){
+        .height-58 {
+            height: 60vh;
+        }
+      }
 
+      @media (min-width:900px) and (max-width:1400px){
+        .height-58 {
+            height: 70vh;
+        }
+      }
+
+      @media (min-width:1400px) and (max-width:1920px){
+        .height-58 {
+            height: 63vh;
+        }
+      }
+  </style>
 </head>
 <body>
 <!-- partial:index.partial.html -->
@@ -171,7 +194,7 @@
         </div>   
 
         <div class="row">
-          <div class="col-12 col-md-4 col-lg-4">
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/algo.png"> 
@@ -186,7 +209,7 @@
             </div>
           </div>
 
-          <div class="col-12 col-md-4 col-lg-4">
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/mds.png"> 
@@ -202,7 +225,7 @@
             </div>
           </div>
 
-          <div class="col-12 col-md-4 col-lg-4">
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/hft-t.png"> 
@@ -216,11 +239,8 @@
                 Gain a competitive edge in the market with our high-frequency trading (HFT) platform, powered by proprietary architecture and TBT market data solutions, along with flexible customization options. Our unique system architecture delivers the lowest latencies available, enabling lightning-fast trade execution and ensuring you stay ahead of the competition.
               </div>
             </div>
-          </div> 
-        </div>
-
-        <div class="row">
-          <div class="col-12 col-md-4 col-lg-4">
+          </div>  
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/mind-mapping.png"> 
@@ -236,7 +256,7 @@
             </div>
           </div>
 
-          <div class="col-12 col-md-4 col-lg-4">
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/market-analysis.png"> 
@@ -252,7 +272,7 @@
             </div>
           </div>
 
-          <div class="col-12 col-md-4 col-lg-4">
+          <div class="col-12 col-md-6 col-lg-4">
             <div class="card-box-about height-58">
               <div class="text-center mb-5">
                 <img class="w-25" src="assets/img/consulting.png"> 
